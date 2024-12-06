901f2eef97167bfc0ee7d60973c28a75df3b17cc
df-salaz <df.salaz@gmail.com>
2024-12-06 16:24:43 -0700

    Begin part 2

--- 2024/05-print_queue/part-1/src/main.rs
+++ 2024/05-print_queue/part-1/src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use std::ops::ControlFlow;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -17,44 +18,76 @@ fn main() {
 
     let mut total = 0;
 
-    for update in printing_updates.iter() {
-        let mut failed = false;
-        for rule in ordering_rules.iter() {
-            let before = rule[0];
-            let after = rule[1];
-
-            let before_index = update.iter().position(|&page| page == before);
-            match before_index {
-                None => {
-                    continue;
-                },
-                Some(..) => {}
-            }
-            let after_index = update.iter().position(|&page| page == after);
-            match after_index {
-                None => {
-                    continue;
-                },
-                Some(..) => {}
-            }
-
-            if before_index > after_index {
-                println!("Before: {}:{}; After: {}:{}",
-                    before_index.unwrap(),
-                    before,
-                    after_index.unwrap(),
-                    after
-                );
-                failed = true;
-            }
-        }
-        if !failed {
-            let middle = update[update.len()/2];
+    for printing_update in printing_updates.iter() {
+        let success = check_update(printing_update, &ordering_rules);
+        if success {
+            let middle = printing_update[printing_update.len()/2];
             total += middle;
         }
     }
 
+    println!("Part 1:");
     println!("{total}");
+
+    total = 0;
+    for printing_update in printing_updates.iter() {
+        let success = check_update(printing_update, &ordering_rules);
+        if !success {
+            let corrected_update = fix_update(printing_update, &ordering_rules);
+            let middle = corrected_update[corrected_update.len()/2];
+            total += middle;
+        };
+    };
+
+    println!("Part 2:");
+    println!("{total}");
+}
+
+fn fix_update(printing_update: &Vec<usize>, ordering_rules: &Vec<Vec<usize>>) -> Vec<usize> {
+    let mut new_update = printing_update.clone();
+    let mut i = 0;
+    while i < printing_update.len() {
+        for rule in ordering_rules.iter() {
+            if rule[0] == printing_update[i] {
+
+            }
+        }
+        i += 1;
+    };
+    new_update
+}
+
+fn check_update(printing_update: &Vec<usize>, ordering_rules: &Vec<Vec<usize>>) -> bool {
+    let mut failed = false;
+    for rule in ordering_rules.iter() {
+        let good = check_rule(printing_update, &rule);
+
+        if !good {
+            failed = true;
+        };
+    }
+    !failed
+}
+
+fn check_rule(printing_update: &Vec<usize>, rule: &Vec<usize>) -> bool {
+    let before = rule[0];
+    let after = rule[1];
+
+    let before_index = printing_update.iter().position(|&page| page == before);
+    match before_index {
+        None => {
+            return true;
+        },
+        Some(..) => {}
+    };
+    let after_index = printing_update.iter().position(|&page| page == after);
+    match after_index {
+        None => {
+            return true;
+        },
+        Some(..) => {}
+    };
+    before_index <= after_index
 }
 
 fn get_vec_vec(section: &str, pattern: char) -> Vec<Vec<usize>> {
@@ -79,5 +112,4 @@ fn get_sections(input: &String) -> Vec<&str> {
         .collect();
     sections
 }
-
 
