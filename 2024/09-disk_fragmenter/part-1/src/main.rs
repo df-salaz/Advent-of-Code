209102005505b6fd32392a16f33835c05765b02b
df-salaz <df.salaz@gmail.com>
2024-12-09 04:12:56 -0700

    Day 09 Part 2

--- 2024/09-disk_fragmenter/part-1/src/main.rs
+++ 2024/09-disk_fragmenter/part-1/src/main.rs
@@ -1,37 +1,84 @@
-use std::{env, fs::read_to_string};
+use std::{env, fs};
 
 fn main() {
     let input = process_input();
 
-    let fs_map = generate_filesystem(input);
+    let (fs_map, largest_id) = generate_filesystem(input);
 
-    let contiguous = part_1(fs_map.clone());
-    let contiguous_checksum = get_checksum(contiguous);
-    println!("{contiguous_checksum}");
+    let contiguous = part_1(&fs_map);
+    println!("Part 1: {}", get_checksum(contiguous));
 
-    //let defrag = part_2(fs_map.clone());
-    //let defrag_checksum = get_checksum(defrag);
-    //println!("{defrag_checksum}");
+    let defrag = part_2(&fs_map, largest_id);
+    println!("Part 2: {}", get_checksum(defrag));
 }
 
-fn part_2(fs_map: Vec<Option<i64>>) -> Vec<Option<i64>> {
-    todo!()
+fn part_2(fs_map: &Vec<Option<i64>>, largest_id: i64) -> Vec<Option<i64>> {
+    let mut filesystem: Vec<Option<i64>> = fs_map.clone();
+    for id in (0..=largest_id).rev() {
+        let size = filesystem
+            .iter()
+            .rev()
+            .filter(|block| **block == Some(id))
+            .count();
+        match filesystem
+            .iter()
+            .enumerate()
+            // find the start index of a contiguous empty block where we fit
+            .position(|(i, block)| {
+                match block {
+                    Some(_) => return false,
+                    None => (),
+                };
+                for j in (i + 1)..(i + size) {
+                    if j >= filesystem.len() { return false };
+                    match filesystem[j] {
+                        Some(_) => return false,
+                        None => continue,
+                    };
+                };
+                true
+            }) {
+                Some(index) => {
+                    let file_index = match filesystem
+                        .iter()
+                        .position(|block| {
+                            *block == Some(id)
+                        }) {
+                            Some(v) => v,
+                            None => continue,
+                        };
+                    // only move files left
+                    if index > file_index { continue };
+                    for i in file_index..(file_index + size) {
+                        filesystem[i] = None;
+                    }
+                    for i in index..(index + size) {
+                        filesystem[i] = Some(id);
+                    }
+                },
+                // do nothing if we don't fit anywhere
+                None => continue,
+            };
+    };
+    filesystem
 }
 
-fn part_1(fs_map: Vec<Option<i64>>) -> Vec<Option<i64>> {
-    let mut fs_map = Box::new(fs_map);
-    for block in Box::clone(&fs_map).iter_mut().rev() {
+fn part_1(fs_map: &Vec<Option<i64>>) -> Vec<Option<i64>> {
+    let mut fs_map = fs_map.clone();
+    loop {
+        // look for an empty block
         let index = match fs_map.iter().position(|block| *block == None) {
             Some(val) => val,
+            // if we don't find one, we have succeeded in making the filesystem contiguous
             None => break,
         };
-        fs_map[index] = *block;
-        fs_map.pop();
+        // move the last block to the empty block
+        fs_map[index] = fs_map.pop().unwrap();
    }
-    *fs_map
+   fs_map
 }
 
-fn generate_filesystem(input: String) -> Vec<Option<i64>> {
+fn generate_filesystem(input: String) -> (Vec<Option<i64>>, i64) {
     let mut fs_map: Vec<Option<i64>> = Vec::new();
     let mut id = 0;
     for (i, c) in input.chars().enumerate() {
@@ -44,14 +91,17 @@ fn generate_filesystem(input: String) -> Vec<Option<i64>> {
             _ => unreachable!()
         }
     }
-    fs_map
+    (fs_map, id - 1)
 }
 
 fn get_checksum(fs_map: Vec<Option<i64>>) -> i64 {
     let mut total: i64 = 0;
     let mut i: usize = 0;
-    while i < fs_map.len() && fs_map[i] != None {
-        total += i as i64 * fs_map[i].unwrap();
+    while i < fs_map.len() {
+        match fs_map[i] {
+            Some(_) => total += i as i64 * fs_map[i].unwrap(),
+            None => (),
+        }
         i += 1;
     }
     total
@@ -61,8 +111,8 @@ fn process_input() -> String {
     let filename = env::args()
         .last()
         .unwrap();
-    read_to_string(filename)
-        .expect("File not found")
+    fs::read_to_string(filename)
+        .unwrap_or_default()
         .trim()
         .to_string()
 }
